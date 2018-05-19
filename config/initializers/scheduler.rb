4a63874c7d0f88b3d7347fa941092becb672010a
rushilagr <rushil.agrawal@gmail.com>
2018-05-19 22:47:36 +0530

    AutoBot cron job scheduled

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,6 +1,7 @@
 unless defined?(Rails::Console) || File.split($0).last == 'rake'
   s = Scheduler.scheduler
 
-  s.cron ENV['SHEET_JOB_CRON'], name: 'Sheet Job' do
+  s.cron ENV['SHEET_JOB_CRON'], name: 'AutoBot' do
+    CreateProgramsAndParticipantsFromSheet.call
   end
 end
