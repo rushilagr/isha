c33c3233594e57ea5f4498bf8577b3102ac32d68
rushilagr <rushil.agrawal@gmail.com>
2018-05-20 00:07:47 +0530

    Merge into autobot scheduler commit

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,7 +1,7 @@
 unless defined?(Rails::Console) || File.split($0).last == 'rake'
   s = Scheduler.scheduler
 
-  s.cron ENV['SHEET_JOB_CRON'], name: 'AutoBot' do
+  s.cron ENV['SHEET_JOB_CRON'], name: 'AutoBot', overlap: false do
     CreateProgramsAndParticipantsFromSheet.call
   end
 end
