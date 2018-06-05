db0c7c10e91083c90a0d79131e0c5595b7a70cb0
rushilagr <rushil.agrawal@gmail.com>
2018-06-05 23:44:49 +0530

    Program order corrected

--- app/models/program.rb
+++ app/models/program.rb
@@ -10,8 +10,8 @@ class Program < ApplicationRecord
   has_many :participants, through: :program_participants
   has_many :temp_participants
 
-  scope :registrable, -> { where('starts_at >= ? AND starts_at <= ?', Date.current, 7.days.from_now) }
-  scope :attendanceable, -> { where('ends_at >= ?', 7.days.ago).joins(:program_participants) }
+  scope :registrable, -> { where('starts_at >= ? AND starts_at <= ?', Date.current, 7.days.from_now).order(:starts_at) }
+  scope :attendanceable, -> { where('ends_at >= ?', 7.days.ago).joins(:program_participants).distinct.order(:starts_at) }
 
   def self.type_enum
     ['Yogasanas', 'Bhuta Shuddhi', 'Inner Engineering - 4 Days', 'Surya Kriya', 'Inner Engineering', 'Angamardana', 'Guru Pooja Training', 'Hatha Yoga 21 Days', 'Isha Yoga for Children']
