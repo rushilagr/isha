ab5664384a93ce732419ffd457e69b167523ce83
rushilagr <rushil.agrawal@gmail.com>
2018-10-11 19:52:13 +0530

    Devise after sign and sign out redirect paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,4 +38,17 @@ class ApplicationController < ActionController::Base
       build_ransack_search Participant
     end
   end
+
+  ## --------------------------------------------
+  ## Devise after sign in & out paths
+  ## --------------------------------------------
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) ||
+    resource.volunteer? ? consume_call_tasks_path : signed_in_root_path(resource)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 end
