20a9868fd633d3742e5d4d3831ba523bac3e167b
rushilagr <rushil.agrawal@gmail.com>
2019-01-11 17:21:02 +0530

    Default sign in path bug fixed

--- app/controllers/concerns/devise_helpers.rb
+++ app/controllers/concerns/devise_helpers.rb
@@ -7,7 +7,11 @@ module DeviseHelpers
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) ||
-    resource.volunteer? ? consume_call_tasks_path : signed_in_root_path(resource)
+    if resource.is_a?(User) && resource.volunteer?
+      consume_call_tasks_path
+    else
+      super
+    end
   end
 
   def after_sign_out_path_for(resource_or_scope)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-
   root to: 'home#root'
 
 
